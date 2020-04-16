85e8515a28883c7987fdc28b4c165b82d4dc9f3e
Erik Magnusson <magnusserik@gmail.com>
2020-04-16 18:07:24 +0200

    Plotting austria searches on corona and infected in same plot

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Skiping row 1 and 2 since they only contain coordinates of countries
-df = pd.read_csv('data/time_series_covid19_confirmed_global.csv', index_col=0, skiprows=[1,2])
-print(df)
+df_confirmed = pd.read_csv('data/time_series_covid19_confirmed_global.csv', index_col=0, skiprows=[1,2])
+df_search = pd.read_csv('data/time_serie_coronavirus_searches.csv', index_col=0)
+print(df_search)
 
 
-df = pd.DataFrame(df, columns=['Denmark','Austria', 'Sweden'])
-print(df)
-df.plot()
+df_confirmed_country = pd.DataFrame(df_confirmed, columns=['Austria'])
+df_search_country = pd.DataFrame(df_search, columns=['Austria'])
+df = df_confirmed_country.merge(df_search_country, left_index=True, right_index=True)
+# plt.figure()
+
+# https://pandas.pydata.org/pandas-docs/version/0.13/visualization.html
+df['Austria_y'] = df['Austria_y'].astype(str).astype(int)
+
+y = df.plot(secondary_y=['Austria_y'])
+y.set_ylabel('Infected')
+y.right_ax.set_ylabel('Search amount')
 plt.show()
